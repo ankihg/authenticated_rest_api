c99f0510c881898f142fb31a1c8f388b8ed4d082
ankihg <hagelin.annika@gmail.com>
2016-03-22 14:59:46 -0700

    authentification and server tests

--- routes/login.js
+++ routes/login.js
@@ -10,14 +10,16 @@ module.exports = (router, models) => {
     let authArr = req.headers.authorization.split(' ');
     if (authArr[0] !== 'basic') return res.status(400).send('basic authentication only');
     authArr = new Buffer(authArr[1], 'base64').toString().split(':');
+
     let username = authArr[0];
     let password = authArr[1];
 
     User.findOne({name:username}, (err, user) => {
       if (err) return res.status(500).send('error finding username');
-      if (!user) return res.status(400).send(`user ${username} does not exist`);
+      if (!user) return res.status(400).json({msg:`user does not exist`});
+
       let valid = user.compareHash(password);
-      if (!valid) return res.status(200).send('failure to login');
+      if (!valid) return res.status(200).json({msg:'invalid login'});
       res.status(200).json({token:user.generateToken()}).end();
     });
   });
