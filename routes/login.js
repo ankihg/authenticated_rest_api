1e8651b5afc88ae77e9ce2ab95feb009f819fef0
ankihg <hagelin.annika@gmail.com>
2016-03-22 09:49:04 -0700

    got username and password from login post

--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,13 @@ module.exports = (router, models) => {
 
   router.post('/login', (req, res) => {
     console.log('plz login');
-    console.log(req.headers.authorization);
+    let authArr = req.headers.authorization.split(' ');
+    if (authArr[0] !== 'basic') return res.status(400).send('basic authentication only');
+    authArr = new Buffer(authArr[1], 'base64').toString().split(':');
+    let username = authArr[0];
+    let password = authArr[1];
+    console.log(username);
+    console.log(password);
   });
 
 };
